0c80563cb4eed06ae24736344dc65e701b41a045
yuraKh <yurakhodanitcky@gmail.com>
2015-03-24 22:36:51 +0200

    0.0.0.2

--- Assets/Scripts/Camera1.cs
+++ Assets/Scripts/Camera1.cs
@@ -1,28 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Camera1 : MonoBehaviour {
+public class Camera1 : MonoBehaviour
+{
+Camera Cam1 = GameObject.Find("Camera 1").GetComponent<Camera>();
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	if (Input.GetMouseButton(0)) {
-			transform.position += new Vector3 (Input.GetAxis ("Mouse X"), Input.GetAxis ("Mouse Y"), 0);
-		}
-		if (Input.GetMouseButton(1)) {
-			//transform.rotation = new Quaternion(Input.GetAxis ("Mouse X"), 0, 0, 0);
-			transform.Rotate(Input.GetAxis ("Mouse Y"),Input.GetAxis ("Mouse X"),0f);
-		}
-		if (Input.GetKey(KeyCode.F1)) {
-
-			//GameObject.Find("Camera 1").GetComponent<Camera>().rect
-			GameObject.Find("Camera 2").transform.position -= new Vector3(0.01f, 0, 0);
-		}
-		//Camera.main.rect.
-		//Vector4.t
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            transform.position += new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0);
+        }
+        if (Input.GetMouseButton(1))
+        {
+            transform.Rotate(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0f);
+        }
+        if (Input.GetKey(KeyCode.F1))
+        {
+            Vector4 GGg = new Vector4(Cam1.rect.x, Cam1.rect.y, Cam1.rect.height, Cam1.rect.width);
+        /*    float camX = Cam1.rect.x;
+            float camY = Cam1.rect.y;
+            float camW = Cam1.rect.width;
+            float camH = Cam1.rect.height;*/
+            Cam1.rect = Vector4.MoveTowards(Vector4())
+         
+          //  Vector4 camStop = new Vector4(0, 0, 1f, 1f);
+        /* GameObject.Find("Camera 1").GetComponent<Camera>().rect*/ 
+          
+          // GameObject.Find("Camera 1").GetComponent<Camera>().rect.x      
+        }
+       // Camera.main.rect = 
+        
+    }
 }
---
+++ Assets/Scripts/EarthMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EarthMove : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject.Find("Moon").transform.Rotate(0, 0.3f, 0);
+        GameObject.Find("Earth").transform.Rotate(0, -0.1f, 0);
+        transform.RotateAround(Vector3.zero, Vector3.up, 50 * Time.deltaTime);
+        //transform.Rotate(Vector3.right * Time.deltaTime);
+    }
+}
